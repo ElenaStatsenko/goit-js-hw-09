487e3a2754e2f67b3b4438e6f17ecba7a88f5740
Mykhailo Statsenko <mihail.prk@gmail.com>
2023-06-10 17:55:59 +0300

    добавила календарь

---
+++ src/js/02-timer.js
@@ -0,0 +1,25 @@
+
+
+import flatpickr from "flatpickr";
+
+import "flatpickr/dist/flatpickr.min.css";
+
+flatpickr("input#datetime-picker", {});
+        
+  
+
+const timer ={
+    start() {
+        const startTime = Date.now();
+        console.log(startTime);
+
+        setInterval(() =>{
+            const currentTime = Date.now();
+            
+
+        }
+
+        )
+    }
+}
+
