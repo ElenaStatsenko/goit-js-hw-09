27f1308ae837ec78fa68031e8aa2477113e3e644
Mykhailo Statsenko <mihail.prk@gmail.com>
2023-06-11 19:20:35 +0300

    начала делать вторую задачу

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,22 +4,55 @@ import flatpickr from "flatpickr";
 
 import "flatpickr/dist/flatpickr.min.css";
 
-flatpickr("input#datetime-picker", {});
-        
-  
 
-const timer ={
-    start() {
-        const startTime = Date.now();
-        console.log(startTime);
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+  };  
+flatpickr("input#datetime-picker", options);
+   console.log(options);    
+const refs = {
+    datePiker: document.querySelector('#datetime-picker'),
+    btnStart: document.querySelector('button[data-start]'),
+    timer: document.querySelector('.timer'),
+};
 
-        setInterval(() =>{
-            const currentTime = Date.now();
+   refs.btnStart.addEventListener('click', onClick);
+
+   function onClick(){
+
+    
+    const currentDate = new Date();
+    console.log(currentDate);
+
+
+    const inputDate = refs.datePiker.value;
+    console.log(refs.datePiker.value);
+
+
+    const dateForHTML = inputDate - currentDate;
+    console.log(dateForHTML);
+    
+
+   }
+
+// const timer ={
+//     start() {
+//         const startTime = Date.now();
+//         console.log(startTime);
+
+//         setInterval(() =>{
+//             const currentTime = Date.now();
             
 
-        }
+//         }
 
-        )
-    }
-}
+//         )
+//     }
+// }
 
