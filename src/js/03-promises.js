3cb14e0986a681c55a36b6ab6612382f081d60e5
Mykhailo Statsenko <mihail.prk@gmail.com>
2023-06-15 22:42:32 +0300

    начала делать третью задачу

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,25 @@
+const form =document.querySelector('.form'); 
+console.log(form);
+
+form.addEventListener('submit', createPromise);
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+  setTimeout(() => { if (shouldResolve) {
+    resolve("Success! Value passed to resolve function");
   } else {
-    // Reject
+    reject("Error! Error passed to reject function");
   }
+}, ${delay});
 }
+
+
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
